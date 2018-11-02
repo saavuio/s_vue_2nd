316416d07bcd98240f7abb0e70da7329c0764240
Nikke Leskelä <nileskel@gmail.com>
2018-11-02 09:42:00 +0000

    Change lint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-self-closing': 'always',
   },
   parserOptions: {
     parser: 'typescript-eslint-parser',
