abaaa426b88afcc0636b065175e23f8ccaca70c5
Nikke Leskelä <nileskel@gmail.com>
2018-11-07 17:48:41 +0000

    Fix bundle name.

--- templates/scripts/bundle.sh
+++ templates/scripts/bundle.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $SCRIPT_DIR
 cd ..
 
-NAME=SaavuHandsontable
+NAME=ExampleBundleName
 
 ./s_vue_2nd.sh yarn run build --target lib --name $NAME src/$NAME.vue
