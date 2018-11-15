e45f0a5c3477f6271f239733530e8abd759958dd
Nikke Leskelä <nileskel@gmail.com>
2018-11-15 08:05:51 +0000

    Fix dev.sh

--- templates/scripts/dev.sh
+++ templates/scripts/dev.sh
@@ -4,4 +4,10 @@ cd $SCRIPT_DIR
 cd ..
 
 . ./env-development
-PTO=$DEV_PORT ./s_vue_2nd.sh yarn run serve --port $DEV_PORT
+
+CONTAINER_NAME_PREFIX=example-app-ui
+
+# run
+CONTAINER_NAME=$CONTAINER_NAME_PREFIX-app \
+  PORT_TO_OPEN=$DEV_PORT \
+  NODE_ENV=development ./s_vue_2nd.sh yarn run serve --port $DEV_PORT
