506ca4b13f228e40222362abdb2351ba57752a19
Nikke Leskelä <nileskel@gmail.com>
2018-11-21 17:56:36 +0000

    Add public dir. Add dockerstop funcion.

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -24,6 +24,13 @@ if [ ! -f $RUN_DIR/package.json ]; then
   exit 1
 fi
 
+dockerstop() {
+  docker stop -t 0 $(docker ps | grep $CONTAINER_NAME | cut -d' ' -f1) > /dev/null 2>&1
+}
+if [ -z $RUN_IN_BG ]; then
+  trap 'dockerstop' SIGINT SIGTERM EXIT
+fi
+
 # Create placeholders for folders that should exist so that
 # permissions will be correct when they are mounted to the
 # container.
@@ -56,6 +63,7 @@ docker run \
   $([ -f $RUN_DIR/env-development ] && echo "--volume $RUN_DIR/env-development:/s_vue_2nd/env-development") \
   $([ -d $RUN_DIR/src ] && echo "--volume $RUN_DIR/src:/s_vue_2nd/src") \
   $([ -d $RUN_DIR/dist ] && echo "--volume $RUN_DIR/dist:/s_vue_2nd/dist") \
+  $([ -d $RUN_DIR/public ] && echo "--volume $RUN_DIR/public:/s_vue_2nd/public") \
   $([ -f $RUN_DIR/vue.config.js ] && echo "--volume $RUN_DIR/vue.config.js:/s_vue_2nd/vue.config.js") \
   --workdir $WORKDIR \
   --entrypoint sh \
