57fc70dd56c36c4f2c644c640639e515395bd71b
Nikke Leskelä <nileskel@gmail.com>
2018-12-02 13:58:06 +0000

    More attributes-per-line.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,10 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/html-self-closing': 'always',
+    "vue/max-attributes-per-line": [2, {
+      "singleline": 2,
+      "multiline": { "max": 2, "allowFirstLine": false },
+    }]
   },
   parserOptions: {
     parser: 'typescript-eslint-parser',
