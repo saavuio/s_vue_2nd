603f722bb481743bbb0cfbb40c30aeb5efd5b267
Nikke Leskelä <nileskel@gmail.com>
2018-12-07 13:12:44 +0000

    Update eject.sh template.

--- templates/scripts/eject.sh
+++ templates/scripts/eject.sh
@@ -7,7 +7,12 @@ if [ ! -f $RUNNER ]; then exit 1; fi
 CONTAINER_NAME=${IMAGE_NAME}_tmp
 CONTAINER_NAME=$CONTAINER_NAME NO_TTY=1 $RUNNER eval "sleep 30;" &
 
-sleep 3
+echo "run container..."
+until docker ps | grep "$CONTAINER_NAME" > /dev/null; do
+  sleep 2
+  echo "waiting for container to start..."
+done
+echo "container started!"
 
 docker cp $CONTAINER_NAME:/$IMAGE_NAME/node_modules ./
 docker cp $CONTAINER_NAME:/$IMAGE_NAME/.eslintrc.js ./
