4f8ee936309370235d7b85879efe1625e9d3fac8
Nikke Leskelä <nileskel@gmail.com>
2018-12-23 08:35:55 +0000

    eject.sh updated.

--- templates/scripts/eject.sh
+++ templates/scripts/eject.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR
+cd ..
+
 echo "Fetching stuff from inside the container."
 
-IMAGE_NAME=s_vue_2nd
-RUNNER=./$IMAGE_NAME.sh
+IMAGE_NAME=s_node_1st
+RUNNER=./${IMAGE_NAME}.sh
 if [ ! -f $RUNNER ]; then exit 1; fi
 CONTAINER_NAME=${IMAGE_NAME}_tmp
 CONTAINER_NAME=$CONTAINER_NAME NO_TTY=1 $RUNNER eval "sleep 30;" &
@@ -14,7 +18,10 @@ until docker ps | grep "$CONTAINER_NAME" > /dev/null; do
 done
 echo "container started!"
 
-docker cp $CONTAINER_NAME:/$IMAGE_NAME/node_modules ./
-docker cp $CONTAINER_NAME:/$IMAGE_NAME/.eslintrc.js ./
-docker cp $CONTAINER_NAME:/$IMAGE_NAME/.prettierrc ./
-docker cp $CONTAINER_NAME:/$IMAGE_NAME/tsconfig.json ./
+docker cp ${CONTAINER_NAME}:/$IMAGE_NAME/node_modules ./
+docker cp ${CONTAINER_NAME}:/$IMAGE_NAME/.eslintrc.js ./
+
+if [ $? -eq 0 ]; then
+  echo "files copied from container!"
+  docker stop ${CONTAINER_NAME}
+fi
