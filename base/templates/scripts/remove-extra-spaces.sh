2dd088b42842a06850fae8dfc791d210f7e8238c
Nikke Leskelä <nileskel@gmail.com>
2018-12-23 09:34:25 +0000

    Update space removal script.

--- base/templates/scripts/remove-extra-spaces.sh
+++ base/templates/scripts/remove-extra-spaces.sh
@@ -4,4 +4,7 @@ cd $SCRIPT_DIR
 cd ..
 cd src
 
+# remove leading spaces
 find . -type f -print0 | xargs -0 perl -pi -e 's/ +$//'
+# remove non-breaking spaces
+find . -type f -print0 | xargs -0 perl -pi -e 's/ / /g'
