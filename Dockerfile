7f130a4cea6acc03b019272fb343f37d4cf702f9
Nikke Leskelä <nileskel@gmail.com>
2019-02-07 12:26:08 +0000

    Add dev arg to Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@
 FROM node:8.11.3
 
 ARG container_user_id
+ARG is_dev
 
 RUN useradd --shell /bin/bash -u $container_user_id -o -c "" -m user-in-container
 
 # install vim to help testing inside container
-RUN apt-get update && apt-get install -y vim
+RUN if [ "$is_dev" = "true" ]; then apt-get update && apt-get install -y vim; fi
 
 RUN mkdir -p /s_vue_2nd/src && mkdir -p /ext/node_modules
 
--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -13,7 +13,13 @@ echo "--------------------------------------------------------------------------
 echo
 echo
 
+IS_DEV="false"
+if [ "$1" = "dev" ]; then
+  IS_DEV="true"
+  echo "RUNNING AS A DEV BUILD!"
+fi
+
 # The reason for this complexity is that we want the container to be running
 # with the exact same user id than the host. That way, when stuff gets created
 # inside the container, the host user will own them.
-docker build -t saavu-local/s_vue_2nd_v6 --build-arg container_user_id=$(id -u) .
+docker build -t saavu-local/s_vue_2nd_v6 --build-arg container_user_id=$(id -u) --build-arg is_dev=$IS_DEV .
