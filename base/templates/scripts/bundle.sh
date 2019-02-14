c57d7fbe37b57701068c9805aa4b0d476178852b
Matt Gould <matt@saowapan.com>
2019-02-14 22:13:35 +0700

    feat: allow different commands in bundle.sh for staging and preview.
    Falls back to 'build' and won't mess with anyone elses setup.

--- base/templates/scripts/bundle.sh
+++ base/templates/scripts/bundle.sh
@@ -3,4 +3,10 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $SCRIPT_DIR
 cd ..
 
-./s_vue_2nd.sh yarn run build
+if [ -z $1 ]; then 
+  CMD='build'
+else
+  CMD=$1
+fi
+
+./s_vue_2nd.sh yarn run $CMD
